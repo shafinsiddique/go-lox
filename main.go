c6dcdc3a189f52a497069b412035a3cf971375c2
Shafin Siddique <t-shsidd@microsoft.com>
2020-07-26 03:57:54 -0400

    file reading logic

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
-package go_lox
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func startInterpretation(fileName string) {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Printf("usage: go-lox <script>")
+		return
+	}
+
+	startInterpretation(args[1])
+
+}
